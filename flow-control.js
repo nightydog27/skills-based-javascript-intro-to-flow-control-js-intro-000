b4b5f731fce02ae8d7cb8d2b31101403fb233a2c
Jon C <nightydog27@hotmail.com>
2021-01-26 03:44:39 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -4,17 +4,19 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-  if ( age >= 13 && age <= 19)
+  if (age >= 13 && age <= 19)
   return "You are a teenager!";
-} else {
-  if ( age <= 13 && age <= 19)
-  return "You are not a teenager"
+} else if (age <= 13 && age <= 19)
+return "You are not a teenager";
 }
 
-}
-
-function ageChecker(age) { "You are not a teenager."
-
+function ageChecker(age) {
+  if (age >= 13 && age <= 19)
+  return "You are a teenager!";
+} else if (age <= 12)
+return "You are a kid";
+} else (age >= 19)
+return "You are a grownup";
 }
 
 function ternaryTeenager(age) { "You are a kid"
